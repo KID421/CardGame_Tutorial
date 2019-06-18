6cb192bfe0b9e0ab14509a10afedd2c6e247c214
KID421 <suchild7@gmail.com>
2019-06-18 21:35:05 +0800

    完成卡片生成與判斷勝負

---
+++ CardGame_Tutorial/Assets/Card.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Card
+{
+    private int _number;
+    private float _x;
+    private Sprite _image;
+
+    public int number { get => _number; }
+    public float x { get => _x; set => _x = value; }
+    public Sprite image { get => _image; }
+
+    public Card(int getNumber, float getX, Sprite getImage)
+    {
+        _number = getNumber;
+        _x = getX;
+        _image = getImage;
+        GameObject temp = new GameObject("卡片");
+        temp.AddComponent<SpriteRenderer>();
+        temp.GetComponent<SpriteRenderer>().sprite = image;
+        temp.transform.position = new Vector2(x, 0);
+    }
+}
---
+++ CardGame_Tutorial/Assets/CardManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public Sprite[] images;
+
+    private Card cardPlayer, cardPC;
+
+    public TextMesh tip;
+
+    private void Start()
+    {
+        int rPlayer = Random.Range(1, 10);
+        cardPlayer = new Card(rPlayer, -5, images[rPlayer - 1]);
+
+        int rPC = Random.Range(1, 10);
+        cardPC = new Card(rPC, 5, images[rPC - 1]);
+
+        if (cardPlayer.number > cardPC.number)
+        {
+            tip.text = "獲勝";
+        }
+        else if (cardPlayer.number < cardPC.number)
+        {
+            tip.text = "失敗";
+        }
+        else
+        {
+            tip.text = "平手";
+        }
+    }
+}
